a6a50e2d82359fceb2434f0a13c9e6e86000321d
Alejandra Gonzalez <ale.bonilla309@gmail.com>
2023-11-11 15:23:56 +0100

    cambio ejercicio 8

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -3,37 +3,33 @@ import java.util.Scanner;
 public class Ejercicio8 {
     public static void main(String[] args) {
         Scanner calculo= new Scanner(System.in);
-        System.out.println("1. caracter +");
-        System.out.println("2. Caracter -");
-        System.out.println("3. Caracter /");
-        System.out.println("4. Caracter *");
-        System.out.println("Que opcion quieres realizar:");
-        int opcion= calculo.nextInt();
-        int operando1= 0, operando2= 0;
-        double resultado= 0.0;
 
-        if (opcion>=1 && opcion<=4){
-            System.out.println("Introduce el numero1:");
-         operando1= calculo.nextInt();
+        System.out.println("Que opcion quieres realizar (+,-,*,/)");
+        char caracter= calculo.next().charAt(0);
+
+
+        System.out.println("Introduce el numero1:");
+          int operando1= calculo.nextInt();
         System.out.println("Introduce el numero2:");
-        operando2= calculo.nextInt();
-        }
+        int operando2= calculo.nextInt();
+        int resultado= 0;
 
-        switch (opcion){
-            case 1:
+
+        switch (caracter){
+            case '+':
                 System.out.println("Vas a sumar");
                 resultado= operando1+operando2;
 
                 break;
-            case 2:
+            case '-':
                 System.out.println("Vas a restar");
                 resultado= operando1-operando2;
                 break;
-            case 3:
+            case '/':
                 System.out.println("Vas a dividir");
-                resultado= (double) operando1/operando2;
+                resultado=  operando1/operando2;
                 break;
-            case 4:
+            case '*':
                 System.out.println("Vas a multiplicar");
                 resultado= operando1*operando2;
                 break;
@@ -41,13 +37,12 @@ public class Ejercicio8 {
                 System.out.println("La opcion no es valida ");
                 break;
         }
-        if (opcion>=1 && opcion<=4){
-            System.out.println("El resultado es: "+resultado);
-        }
-
-
+        System.out.println("El resultado es: "+resultado);
 
 
 
     }
-}
+
+
+    }
+
